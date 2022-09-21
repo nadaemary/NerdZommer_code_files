cddda0e9177dca264bddde62fb61cdf99e170cbd
nadaemary <97479779+nadaemary@users.noreply.github.com>
2022-09-21 22:15:22 +0200

    Update XO.cpp

--- CPP/XO.cpp
+++ CPP/XO.cpp
@@ -1,15 +1,68 @@
-const int X = 1;
+#include <cstdlib>              // system definitions
+#include <iostream>             // I/O definitions
+using namespace std;            // make std:: accessible
 
-const int Y = -1;
+const int X = 1, O = -1, EMPTY = 0;  // possible marks
+int board[3][3];                     // playing board
+int currentPlayer;                   // current plaryer
 
-const int EMPTY = 0;
-
-int currentPlayer = X;
-
-void inputMark(int i, int;) {                      // the input function
-
-    board[i][j] = currentPlayer;
-
-   currentPlayer = -currentPlayer;
-
+void clearBoard() {
+  for (int i = 0; i< 3; i++){
+    for (int j =0; j < 3; j++){
+      board[i][j] = EMPTY;
+    }
+  }
+  currentPlayer = X;
 }
+
+void inputMark(int i, int j){
+  board[i][j] = currentPlayer;
+  currentPlayer = -currentPlayer;
+}
+
+bool isWin(int mark) {                             // is mark the winner
+  int win = 3*mark;                               // +3 for X and -3 for O
+  return ((board[0][0] + board[0][1] + board[0][2] == win) // row 0
+|| (board[1][0] + board[1][1] + board[1][2] == win) // row 1
+|| (board[2][0] + board[2][1] + board[2][2] == win) // row 2
+|| (board[0][0] + board[1][0] + board[2][0] == win) // column 0
+|| (board[0][1] + board[1][1] + board[2][1] == win) // column 1
+|| (board[0][2] + board[1][2] + board[2][2] == win) // column 2
+|| (board[0][0] + board[1][1] + board[2][2] == win) // diagonal
+|| (board[2][0] + board[1][1] + board[0][2] == win) // diagonal
+);
+}
+
+void getWinner() {
+  if (isWin(X)) cout<<"Player X won"<< endl;
+  else if (isWin(O)) cout<<"Player O won"<< endl;
+  else return cout<<"Tie"<< endl;
+}
+
+void printBoard() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      switch (board[i][j]) {
+        case X: cout<< "X"; break;
+        case O: cout<< "O"; break;
+        case EMPTY: cout<< " "; break;
+      }
+      if (j < 2) cout<< "|";
+    }
+    if (i < 2) cout<< "\n-+-+-\n";
+  }
+}
+
+int main() {
+  clearBoard();
+  putMark(0,0);
+  putMark(1,1);
+  putMark(0,1);
+  putMark(0,2);
+  putMark(2,0);
+  putMark(1,2);
+  putMark(2,2);
+  putMark(2,1);
+  putMark(1,0);
+  printBoard();
+  getWinner();
